9198f413cc50413fa695a990315ace73f860fdb0
Akshat Kashyap <akshat@Akshats-MacBook-Air.local>
2023-09-25 09:33:34 +0530

    Solution

--- merge-two-sorted-lists.py
+++ merge-two-sorted-lists.py
@@ -5,15 +5,20 @@ class Solution:
         nlist = dummy
 
         while list1 and list2:
+            
             if list1.val < list2.val:
                 nlist.next = list1
                 list1 = list1.next
+            
             else:
                 nlist.next = list2
                 list2 = list2.next
+            
             nlist = nlist.next
 
+        
         if list1:
             nlist.next = list1
+        
         elif list2:
             nlist.next = list2
